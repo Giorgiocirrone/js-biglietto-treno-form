ecfc0b61ce748e76a655585c6a2bc7fe21f3bd1b
Giorgiocirrone <cirronegiorgio@gmail.com>
2024-08-07 14:30:38 +0200

    impostazione tabella esercizio

---
+++ js/script.js
@@ -0,0 +1,42 @@
+/*
+-Raccolgo gli elementi di interesse del Dom
+-Faccio reagire al click il bottone di conferma 
+
+
+|Nel momento in cui clicco 
+
+
+-Recupero i valori dal form
+
+fase di validazione 
+
+-calcolo del prezzo in base ai km 
+-verifico se applicare lo sconto 
+-arrotondo a 2 decimali 
+
+svuoto i campi dal form
+
+-preparo i valori corretti nel biglietto 
+-mostro il biglietto
+
+
+*/
+
+
+// definire le variabili del form e del biglietto 
+
+// elementi interenti al fomr 
+const name = document.getElementById('name');
+const Kms = document.getElementById('Kms');
+const age = document.getElementById('age');
+const confirmButton = documet.getElementById('confirm-button');
+
+
+//elementi inerenti al biglietto 
+
+
+
+
+
+
+
